670c78033916c818ea102bda8e45a71ac3307174
Punker58 <mistershowoff.com@gmail.com>
2022-12-07 18:12:14 +0100

    0.0.1 - creazione progetto: shop auto jdm

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+export default function Navbar() {
+    return (
+        <>
+
+        <div className='container 2xl'>
+
+            <div className='grid grid-cols-2 gap-4'>
+
+            <div className='columns-5 p-5 m-5 col-start-2 text-white '>
+                <p>LOGO</p>
+                <p>Home</p>
+                <p>Modelli</p>
+                <p>Usato</p>
+                <p>Contatti</p>
+            </div>
+
+            </div>
+
+        </div>
+
+      </>
+
+    );
+}
+
